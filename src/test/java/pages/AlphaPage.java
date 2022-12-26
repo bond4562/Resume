aa16880c4c960c4deaf24beb768eb3559a5a2d93
bond4562 <107379997+bond4562@users.noreply.github.com>
2022-12-26 20:04:58 +0700

    Add files via upload

---
+++ src/test/java/pages/AlphaPage.java
@@ -0,0 +1,11 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import pages.base_abstract.TopBarPage;
+
+public class AlphaPage extends TopBarPage {
+
+    public AlphaPage(WebDriver driver) {
+        super(driver);
+    }
+}
---
+++ src/test/java/pages/base_abstract/LeftSideBarNewsPage.java
@@ -0,0 +1,10 @@
+package pages.base_abstract;
+
+import org.openqa.selenium.WebDriver;
+
+public abstract class LeftSideBarNewsPage extends NewsMenuPage {
+
+    protected LeftSideBarNewsPage(WebDriver driver) {
+        super(driver);
+    }
+}
